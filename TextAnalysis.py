17fdd80728a57e8a51201de1f190756b346017ac
Jesus <jesusalgaq@outlook.com>
2021-03-18 17:49:09 -0600

    tone analyzer setup

---
+++ TextAnalysis.py
@@ -0,0 +1,13 @@
+import Credentials.py
+
+from ibm_watson import ToneAnalyzerV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+#Authenticate
+
+authenticator = IAMAuthenticator(apikey)
+ta = ToneAnalyzerV3(version='2017-09-21', authenticator=authenticator)
+ta.set_service_url(url)
+
+#Analyze Tone
+res = ta.tone('I take care of my skin so there is something shining in here, because my hopes and dreams are not going to do it').get_result()

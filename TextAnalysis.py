b768a6e97b09753592361d3f08fdbc06121b56e8
Jesus <jesusalgaq@outlook.com>
2021-03-18 18:31:18 -0600

    Analysis working

--- TextAnalysis.py
+++ TextAnalysis.py
@@ -1,13 +1,15 @@
-import Credentials.py
+from Credentials import Credentials
 
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 #Authenticate
 
-authenticator = IAMAuthenticator(apikey)
+authenticator = IAMAuthenticator(Credentials.apikey)
 ta = ToneAnalyzerV3(version='2017-09-21', authenticator=authenticator)
-ta.set_service_url(url)
+ta.set_service_url(Credentials.url)
 
 #Analyze Tone
 res = ta.tone('I take care of my skin so there is something shining in here, because my hopes and dreams are not going to do it').get_result()
+
+print (res)

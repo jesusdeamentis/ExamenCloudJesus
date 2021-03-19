f49078e74f1a178e58a69a804ebcf3a4b5f0f5be
Jesus <jesusalgaq@outlook.com>
2021-03-18 22:44:23 -0600

    button correction

--- TextAnalysis.py
+++ TextAnalysis.py
@@ -2,6 +2,7 @@ from Credentials import Credentials
 
 from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+TextToAnalyze = ''
 
 #Authenticate
 
@@ -10,6 +11,6 @@ ta = ToneAnalyzerV3(version='2017-09-21', authenticator=authenticator)
 ta.set_service_url(Credentials.url)
 
 #Analyze Tone
-res = ta.tone('shame on your cow, your life and your ancestors').get_result()
+res = ta.tone(TextToAnalyze).get_result()
 
 print (res)
